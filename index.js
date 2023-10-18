0c7a6b7d179142208ec73d6dd7fc124cf1545b4c
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-18 11:13:27 +0200

    Working on the fight meathod

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ import Fighter from './src/Fighter.js'
 
 const heracles = new Fighter('Heracles', 20, 6);
 const nemeanLion = new Fighter('Nemean Lion', 11, 13);
+
+heracles.fight(nemeanLion);
--- src/Fighter.js
+++ src/Fighter.js
@@ -7,6 +7,22 @@ class Fighter {
         this.dexterity = dexterity;
         this.life = life;
     }
+
+    fight(defender) {
+        let pointsOfAttack = Math.floor(Math.random() * this.strength);
+        let pointsOfDefense = Math.floor(Math.random() * defender.dexterity);
+        let damage = pointsOfAttack - pointsOfDefense;
+        if (damage > 0) {
+            if (defender.life > 0) {
+                if (defender.life - damage <= 0) {
+                    defender.life = 0;
+                }
+                else {
+                    defender.life -= damage;
+                }
+            }
+        }
+    }
 }
 
 export default Fighter;
