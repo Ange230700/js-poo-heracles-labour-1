5207c48a20273330f43eee565db62109741934a3
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-18 12:40:55 +0200

    still working on the game

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import Fighter from './src/Fighter.js'
+import chalk from 'chalk';
 
 const heracles = new Fighter('Heracles', 20, 6);
 const nemeanLion = new Fighter('Nemean Lion', 11, 13);
+let roundNumber = 1;
 
-heracles.fight(nemeanLion);
+while (heracles.life > 0 && nemeanLion.life > 0) {
+    let randomProbability = Math.floor(Math.random() * 2);
+    console.log(`Round #${chalk.green(roundNumber)}\n`);
+    if (randomProbability === 0) {
+        console.log(`\t${chalk.blue(heracles.name)} attacks ${chalk.red(nemeanLion.name)}.\n`);
+        heracles.fight(nemeanLion);
+    }
+    else {
+        console.log(`\t${chalk.red(nemeanLion.name)} attacks ${chalk.blue(heracles.name)}.\n`);
+        nemeanLion.fight(heracles);
+    }
+    roundNumber++;
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,3 +1,5 @@
+import chalk from 'chalk';
+
 const MAX_LIFE = 100;
 
 class Fighter {
@@ -15,11 +17,37 @@ class Fighter {
         if (damage > 0) {
             if (defender.life > 0) {
                 if (defender.life - damage <= 0) {
+                    if (defender.name === 'Heracles') {
+                        console.log(`\t${chalk.blue(defender.name)} has lost ${damage} life points.\n\t${chalk.blue(defender.name)} has currently ${defender.life} life points left.\n\tSo ${chalk.blue(defender.name)} has died.\n`);
+                        defender.life = -1;
+                    }
+                    else {
+                        console.log(`\t${chalk.red(defender.name)} has lost ${damage} life points.\n\t${chalk.red(defender.name)} has currently ${defender.life} life points left.\n\tSo ${chalk.red(defender.name)} has died.\n`);
+                        defender.life = -1;
+                    }
                     defender.life = 0;
                 }
                 else {
                     defender.life -= damage;
+                    if (defender.name === 'Heracles') {
+                        console.log(`\t${chalk.blue(defender.name)} has ${defender.life} life points left.\n\tHe has lost ${damage} life points.\n`);
+                    }
+                    else {
+                        console.log(`\t${chalk.red(defender.name)} has ${defender.life} life points left.\n\tHe has lost ${damage} life points.\n`);
+                    }
                 }
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            damage = 0;
+            if (defender.name === 'Heracles') {
+                console.log(`\t${chalk.blue(defender.name)} has blocked or dodged the attack.\n`);
+            }
+            else {
+                console.log(`\t${chalk.red(defender.name)} has blocked or dodged the attack.\n`);
             }
         }
     }
